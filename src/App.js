800731afbe96456c57a78b09f603322c692a1081
Jegan-kanthasamy <jegankanthasamy@outlook.com>
2023-08-21 17:37:55 +0530

    change color functionality

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const step = 3;
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step === 1 ? "active" : ""}`}>1</div>
-        <div className={`${step === 2 ? "active" : ""}`}>2</div>
-        <div className={`${step === 3 ? "active" : ""}`}>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
       <p className="message">Steps {messages[step - 1]}</p>
